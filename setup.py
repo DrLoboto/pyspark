e202e4714bc00ec350e4baa444eabbbe99cc7806
Valery Vishnevskiy <v.v.vishnevskiy@gmail.com>
2016-03-15 13:27:34 +0600

    Initial commit. Version 1.6.1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(name='pyspark',
+      version='1.6.1',
+      author='Apache Software Foundation',
+      license='	Apache License 2.0',
+      description='PySpark python package',
+      long_description='Library provides high-level APIs in Python and an optimized engine that supports general computation graphs for data analysis.',
+      packages=find_packages())
