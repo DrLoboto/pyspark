b9c12f6bbf8aa155cd125b8b5e854562a330614b
Valery Vishnevskiy <v.v.vishnevskiy@gmail.com>
2016-03-15 13:39:18 +0600

    Add requires

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(name='pyspark',
-      version='1.6.1',
-      author='Apache Software Foundation',
-      license='	Apache License 2.0',
-      description='PySpark python package',
-      long_description='Library provides high-level APIs in Python and an optimized engine that supports general computation graphs for data analysis.',
-      packages=find_packages())
+    version='1.6.1',
+    author='Apache Software Foundation',
+    license='Apache License 2.0',
+    description='PySpark python package',
+    long_description='Library provides high-level APIs in Python and an optimized engine that supports general computation graphs for data analysis.',
+    install_requires=[
+        'py4j'
+    ],
+    packages=find_packages())
